33dbc761fee5cbef91af0f4e3194f5505ee303e0
Narciso Jaramillo <njaramil@adobe.com>
2012-09-12 00:35:13 -0700

    Added ability to specify a branch to pull

--- scripts/pull-updates.sh
+++ scripts/pull-updates.sh
@@ -12,6 +12,9 @@ cd "$tmproot"
 
 /usr/bin/git clone git@github.com:adobe/brackets-updates.git update-repo || { echo "Couldn't clone repo"; exit 1; }
 cd update-repo
+if [ -n "$2" ]; then
+    git checkout $2
+fi
 rm -rf /tmp/updates.old
 if [ -d /var/www/updates ]; then
     mv /var/www/updates /tmp/updates.old
