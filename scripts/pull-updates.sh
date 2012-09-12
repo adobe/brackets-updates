7ea2fec92e9e8e4970a4310bf3c0b2491588bad4
Narciso Jaramillo <njaramil@adobe.com>
2012-09-11 23:47:59 -0700

    Added pull-updates script for running on server

---
+++ scripts/pull-updates.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Getting latest update info..."
+
+if [ -z "$1" ]; then
+    echo "Usage: pull-updates.sh <path-within-repo>"
+    exit 1
+fi
+
+tmproot=`mktemp -d`
+cd "$tmproot"
+
+/usr/bin/git clone git@github.com:adobe/brackets-updates.git update-repo || { echo "Couldn't clone repo"; exit 1; }
+cd update-repo
+rm -rf /tmp/updates.old
+if [ -d /var/www/updates ]; then
+    mv /var/www/updates /tmp/updates.old
+fi
+mv "$1/updates" /var/www/updates
+
+cd /tmp
+rm -rf "$tmproot"
+
+echo "done"
