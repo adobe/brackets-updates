7e39f37efd99f509b5676ec568114e355c290edd
Marcel Gerber <mg.hain@gmx.de>
2014-05-24 21:13:04 +0200

    Only use -k option on Mac

--- scripts/create-gzipped-jsons.sh
+++ scripts/create-gzipped-jsons.sh
@@ -2,6 +2,7 @@
 
 echo "Create gzipped versions of update notification json files"
 TARGET_DIR="`pwd`/GZIPPED-JSONS"
+platform=`uname -s`
 if [ -e ${TARGET_DIR} ]; then
   rm -r ${TARGET_DIR}
 fi
@@ -11,7 +12,12 @@ mkdir ${TARGET_DIR}
 pushd "../updates/stable" > /dev/null
 
 for json in `ls *.json`; do
-  gzip -k -9 < ${json} > ${TARGET_DIR}/${json}
+  echo "Converting $json ..."
+  if [[ "$platform" == "Darwin" ]]; then # Mac OS X
+    gzip -k -9 < ${json} > ${TARGET_DIR}/${json} # Use the -k option to keep the original file
+  else
+    gzip -9 < ${json} > ${TARGET_DIR}/${json} # Windows/Linux don't have the -k option
+  fi
 done
 
 popd > /dev/null
